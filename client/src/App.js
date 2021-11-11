dc0ca668c94f59f48f08bae5f797acfbcf1cb902
ktbg <ktboates@me.com>
2021-11-10 20:34:21 -0500

    screen components set up, react-router installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">Portfolio</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/screens/Projects/Projects.jsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <div>
+      <h1>Check out my projects</h1>
+    </div>
+  );
+};
+
+export default Projects;
